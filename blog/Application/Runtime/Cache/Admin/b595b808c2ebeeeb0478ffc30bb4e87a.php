0243f1ce800e74492a074692e6d8c9809dcae000
zhengml <zhengml@corp.hxqc.com>
2017-03-07 17:10:51 +0800

    H5效果

--- blog/Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
+++ blog/Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -15,6 +15,24 @@
     <script src="/Public/Admin/js/jquery.js"></script>
     <link type="image/x-icon" href="/favicon.ico" rel="shortcut icon" />
     <link href="/favicon.ico" rel="bookmark icon" />
+     <script  src="/Public/Admin/js/canvas-particle.js"></script>
+    <script type="text/javascript">
+        window.onload = function(){
+            var config = {
+                vx: 4,
+                vy:  4,
+                height: 2,
+                width: 2,
+                count: 100,
+                color: "121, 162, 185",
+                stroke: "100,200,180",
+                dist: 6000,
+                e_dist: 20000,
+                max_conn: 10
+            }
+            CanvasParticle(config);
+        }
+    </script>
 </head>
 <body>
 <?php $type="0"?>
